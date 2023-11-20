be47ca5dd01fca2ff092d938ebfbfe1e29535953
Sebi <sebastianstewart03@gmail.com>
2023-11-20 21:28:00 +0000

    Added error handling for Errors: 400,403,404,500,503

--- app.py
+++ app.py
@@ -28,11 +28,33 @@ def index():
 from users.views import users_blueprint
 from admin.views import admin_blueprint
 from lottery.views import lottery_blueprint
+
 #
 # # register blueprints with app
 app.register_blueprint(users_blueprint)
 app.register_blueprint(admin_blueprint)
 app.register_blueprint(lottery_blueprint)
+
+# Error handling for Errors: 400,403,404,500,503
+@app.errorhandler(400)
+def bad_request(error):
+    return render_template('errors/400.html'), 404
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('errors/403.html'), 404
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('errors/500.html'), 404
+
+@app.errorhandler(503)
+def service_unavailable(error):
+    return render_template('errors/503.html'), 404
 
 
 if __name__ == "__main__":
