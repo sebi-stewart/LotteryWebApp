bbca97e070adad138091faf1b4c383bcbc306246
Sebi <sebastianstewart03@gmail.com>
2023-12-01 15:30:10 +0000

    Added logout function and login functionality, stop multiple attempts

--- users/views.py
+++ users/views.py
@@ -1,16 +1,14 @@
 # IMPORTS
 import pyotp
 from flask import Blueprint, render_template, flash, redirect, url_for, session
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 from app import db
 from models import User
 from users.forms import RegisterForm, LoginForm
-from users.data_checks import *
-
+from markupsafe import Markup
 
 # CONFIG
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
-
 
 
 # VIEWS
@@ -28,43 +26,6 @@ def register():
         # if email already exists redirect user back to signup page with error message so user can try again
         if user:
             flash('Email address already exists')
-            return render_template('users/register.html', form=form)
-
-        # Checking user input
-        email = check_email(form.email.data)
-        first_name = check_name(form.firstname.data)
-        last_name = check_name(form.lastname.data)
-        phone = check_phone(form.phone.data)
-        password = check_password(form.password.data)
-        pass_verify = verify_password(form.password.data, form.confirm_password.data)
-        date_of_birth = check_date_of_birth(form.date_of_birth.data)
-        postcode = check_postcode(form.postcode.data)
-
-        # Notifying user if their input was invalid
-        if not email:
-            flash("Email address invalid")
-            return render_template('users/register.html', form=form)
-        if not first_name:
-            flash("First name invalid")
-            return render_template('users/register.html', form=form)
-        if not last_name:
-            flash("Last name invalid")
-            return render_template('users/register.html', form=form)
-        if not phone:
-            flash("Phone number invalid, must be in format XXXX-XXX-XXXX")
-            return render_template('users/register.html', form=form)
-        if not password:
-            flash("Password invalid, must be between 6-12 characters long, contain uppercase, lowercase, number and "
-                  "special character")
-            return render_template('users/register.html', form=form)
-        if not pass_verify:
-            flash("Passwords don't match")
-            return render_template('users/register.html', form=form)
-        if not date_of_birth:
-            flash("Date of birth invalid, must be in format DD/MM/YYYY")
-            return render_template('users/register.html', form=form)
-        if not postcode:
-            flash("Postcode invalid, must be in format 'XY YXX', 'XYY YXX' or 'XXY YXX'")
             return render_template('users/register.html', form=form)
 
         # create a new user with the form data
@@ -116,20 +77,51 @@ def setup_2fa():
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     # Creating login form
+
+    if not session.get('attempts'):
+        # Login attempts
+        session['attempts'] = 0
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        # if email or password doesn't exist, we output the same message
-        if not user or not user.verify_password(form.password.data):
-            flash("Email or Password doesn't exist")
+        if session['attempts'] >= 3:
+            flash(Markup('Number of incorrect login attempts exceeded. '
+                         'Please click <a href="/reset">here</a> to reset.'))
             return render_template('users/login.html', form=form)
 
+        user = User.query.filter_by(email=form.email.data).first()
+        # if email or password doesn't exist, we output the same message
+        if (not user
+                or not user.verify_password(form.password.data)
+                or not True):
+            session['attempts'] += 1
+
+            if session['attempts'] >= 3:
+                flash(Markup('Number of incorrect login attempts exceeded. '
+                             'Please click <a href="/reset">here</a> to reset.'))
+                return render_template('users/login.html', form=form)
+
+            flash('Please check your login details and try again, '
+                  '{} login attempts remaining'.format(3 - session.get('attempts')))
+            return render_template('users/login.html', form=form)
+
+        session['attempts'] = 0
         login_user(user)
 
         return redirect(url_for('users.account'))
 
     return render_template('users/login.html', form=form)
+
+@users_blueprint.route('/reset')
+def reset():
+    session['attempts'] = 0
+    return redirect(url_for('users.login'))
+
+@users_blueprint.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 # view user account
@@ -148,5 +140,3 @@ def account():
                            firstname=current_user.firstname,
                            lastname=current_user.lastname,
                            phone=current_user.phone)
-
-
