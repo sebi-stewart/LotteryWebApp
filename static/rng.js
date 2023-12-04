04f0942ee0e4ef181c4a47b9c6024af36d07854e
Sebi <sebastianstewart03@gmail.com>
2023-12-04 14:50:28 +0000

    Added cryptographically secure number generation, with better rng

--- static/rng.js
+++ static/rng.js
@@ -4,11 +4,21 @@ function luckyDip() {
     // create empty set
     let draw = new Set();
 
+
+
     // while set does not contain 6 values, create a random value between 1 and 60
     while (draw.size < 6) {
         min = 1;
         max = 60;
-        value = Math.ceil(Math.random() * (max - min) + min);
+
+        // Get 1 random number
+        randomBuffer = new Uint32Array(1);
+        window.crypto.getRandomValues(randomBuffer);
+
+        // Better rng for number generation by using Math.floor and adding +1
+        // Making it cryptographically secure by using csRandomNumber instead of Math.random()
+        csRandomNumber = randomBuffer[0] / (0xFFFFFFFF)
+        value = Math.floor(csRandomNumber * (max - min + 1) + min);
 
         // sets cannot contain duplicates so value is only added if it does not exist in set
         draw.add(value)
